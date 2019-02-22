f0f987bf9fe0036f364780828557a2309b7f2b90
konstantin0s <constantintofan85@gmail.com>
2019-02-22 16:05:38 +0100

    Add views and index page

--- .history/app_20190222155839.js
+++ .history/app_20190222155839.js
@@ -15,12 +15,9 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
+
   const index = require('./routes/index');
   app.use('/', index);
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
 
   app.listen(3000, () => {
     console.log('Server started on port 3000');
