ec204dc586aeb043abd53b47dda1d5c59fa29f62
konstantin0s <constantintofan85@gmail.com>
2020-03-20 18:29:11 +0100

    Add/remove class to nav based on device(mobile or desktop)

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,27 +1,41 @@
-//prevent default 
-let form = document.getElementById('form');
+
+let nav = document.getElementById('nav');
 let errorMessage = document.getElementsByClassName('error-message');
 
 
+//add background to nav on mobile
+function mobileBackground() {
+  let docWidth = document.body.clientWidth;
+  let mobileWidth = 700;
+  if (docWidth < mobileWidth) {
+    nav.classList.add('bg-dark');
+  } else {
+    nav.classList.remove('bg-dark');
+  }
+}
+//the magic of adding remove background color on navigation
+window.addEventListener("resize", mobileBackground);
+window.addEventListener("load", mobileBackground);
 
+// mobileBackground();
 
 
 //delete recipe 
-$(document).ready(function() {
-  $('.delete-recipe').on('click', function(e) {
-   $target = $(e.target);
-   const id = $target.attr('data-id');
-   $.ajax({
-     type: 'DELETE',
-     url: '/recipe/'+id,
-     success: function(response) {
-       alert('Deleting recipe');
-       window.location.href='/';
-     },
-     error: function(err) {
-       console.log(err);
-     }
-   });
- });
+$(document).ready(function () {
+  $('.delete-recipe').on('click', function (e) {
+    $target = $(e.target);
+    const id = $target.attr('data-id');
+    $.ajax({
+      type: 'DELETE',
+      url: '/recipe/' + id,
+      success: function (response) {
+        alert('Deleting recipe');
+        window.location.href = '/';
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
+  });
 
 });
