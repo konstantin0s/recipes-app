367316a7e46f937e2e4b12f786817dceb6ea5003
konstantin0s <constantintofan85@gmail.com>
2020-04-12 19:07:48 +0200

    Add Search bar

---
+++ public/javascript/search.js
@@ -0,0 +1,33 @@
+//implement search
+var inputSearch = document.getElementById('input')
+  inputSearch.addEventListener('keyup', () => {
+    this.searchHandler();
+  });
+  
+
+
+function searchHandler(event) {
+  var filter, cards, txtValue;
+  filter = jsUcfirst(inputSearch.value);
+  // console.log(filter)
+  cards = document.getElementById("cards");
+ var li = cards.getElementsByTagName('li');
+
+ for (var i = 0; i < li.length; i++) {
+  var a = li[i].getElementsByTagName("a")[0];
+if (a) {
+  txtValue = a.textContent || a.innerText;
+
+  // console.log(txtValue);
+  if (txtValue.indexOf(filter) > -1) {
+    li[i].style.display = "";
+  } else {
+    li[i].style.display = "none";
+  }
+}
+}
+}
+
+jsUcfirst = str => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
