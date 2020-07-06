7de0113dbb601c1fb50a4443a4c129b97b48ef28
konstantin0s <constantintofan85@gmail.com>
2020-07-06 13:33:41 +0200

    Add progress bar on recipes page

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,20 +1,21 @@
-
 var navbar = document.getElementById('nav');
 // let errorMessage = document.getElementsByClassName('error-message');
 
 
 //add background to navigation on mobile
 function mobileBackground() {
-  let docWidth = document.body.clientWidth;
-  let mobileWidth = 767;
-  if (docWidth < mobileWidth) {
-    navbar.classList.add('bg-dark');
-  } else {
-    navbar.classList.remove('bg-dark');
-  }
+    let docWidth = document.body.clientWidth;
+    let mobileWidth = 767;
+    if (docWidth < mobileWidth) {
+        navbar.classList.add('bg-dark');
+    } else {
+        navbar.classList.remove('bg-dark');
+    }
 }
 //the magic of adding remove background color on navigation
 window.addEventListener("resize", mobileBackground);
 window.addEventListener("load", mobileBackground);
 
 // mobileBackground();
+
+//data help when focus on input
