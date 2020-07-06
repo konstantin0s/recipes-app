1de1a4ad01badd59b84b6c8178dc6ad205aa8987
konstantin0s <constantintofan85@gmail.com>
2020-07-06 13:41:46 +0200

    Add data-help on signup form

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -19,3 +19,14 @@ window.addEventListener("load", mobileBackground);
 // mobileBackground();
 
 //data help when focus on input
+let help = document.querySelector('#help');
+let fields = document.querySelectorAll('input');
+for (let field of Array.from(fields)) {
+    field.addEventListener('focus', event => {
+        let text = event.target.getAttribute('data-help');
+        help.textContent = text;
+    });
+    field.addEventListener('blur', event => {
+        help.textContent = '';
+    });
+}
