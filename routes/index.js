aa95b7b4f1081b001f2be2bfb2bef4c21f19c940
konstantin0s <constantintofan85@gmail.com>
2020-07-22 15:02:47 +0200

    Add favicon icons

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page */
 router.get('/', (req, res) => {
     try {
-        res.render('index');
+        res.render('index', { layout: false });
     } catch (error) {
         console.log(error);
     }
