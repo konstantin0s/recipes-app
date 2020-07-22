947cad63b86f9589b8d4753beb59b48253a6f349
konstantin0s <constantintofan85@gmail.com>
2020-07-22 14:39:48 +0200

    Add jquery ui confirmation on delete recipe

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -2,23 +2,63 @@
 $(document).ready(function() {
     $('.delete-recipe').on('click', function(e) {
         e.stopImmediatePropagation();
-        if (confirm('Are you sure you want to delete the recipe?')) {
-            $target = $(e.target);
 
-            const id = $target.attr('data-id');
-            $.ajax({
-                type: 'DELETE',
-                url: '/recipe/' + id,
-                success: function(response) {
-                    alert('The recipe was deleted');
-                    window.location.href = '/recipes';
-                },
-                error: function(err) {
-                    console.log(err);
-                }
+        let dialogTitle = 'Delete recipe?'
+
+
+        $('<div></div>').appendTo('body')
+            .html('<div><h6>Are you sure about this?</h6></div>').dialog({
+                resizable: false,
+                modal: true,
+                title: dialogTitle || 'Confirm',
+                buttons: {
+                    "Confirm": function() {
+                        // Perform actions here based on
+                        // receiving confirmation
+
+
+
+                        $target = $(e.target);
+
+                        const id = $target.attr('data-id');
+                        $.ajax({
+                            type: 'DELETE',
+                            url: '/recipe/' + id,
+                            success: function(response) {
+                                alert('The recipe was deleted');
+                                window.location.href = '/recipes';
+                            },
+                            error: function(err) {
+                                console.log(err);
+                            }
+                        });
+
+                        // Then close the dialog:
+                        $(this).dialog("close");
+                    },
+                    "Cancel": function() {
+                        $(this).dialog("close");
+                    }
+                } //end buttons option
             });
-        } else {
-            return false;
-        }
+
+        // if (confirm('Are you sure you want to delete the recipe?')) {
+        //     $target = $(e.target);
+
+        //     const id = $target.attr('data-id');
+        //     $.ajax({
+        //         type: 'DELETE',
+        //         url: '/recipe/' + id,
+        //         success: function(response) {
+        //             alert('The recipe was deleted');
+        //             window.location.href = '/recipes';
+        //         },
+        //         error: function(err) {
+        //             console.log(err);
+        //         }
+        //     });
+        // } else {
+        //     return false;
+        // }
     });
 });
