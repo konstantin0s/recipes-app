3abce44cfd942504faab86e781e329811389bd6a
konstantin0s <constantintofan85@gmail.com>
2020-07-30 19:16:26 +0200

    Retry recaptcha on heroku

--- public/javascript/data-help.js
+++ public/javascript/data-help.js
@@ -13,3 +13,21 @@ for (let field of Array.from(fields)) {
         help.textContent = '';
     });
 }
+
+var _captchaTries = 0;
+
+function recaptchaOnload() {
+    _captchaTries++;
+    if (_captchaTries > 9)
+        return;
+    if ($('.g-recaptcha').length > 0) {
+        grecaptcha.render("recaptcha", {
+            sitekey: '6LeR0ZMUAAAAAHe19OLvaksuM7cB-k8XHCCmE96b',
+            callback: function() {
+                console.log('recaptcha callback');
+            }
+        });
+        return;
+    }
+    window.setTimeout(recaptchaOnload, 1000);
+}
